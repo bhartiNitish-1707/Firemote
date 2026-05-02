bde2b76f2d2d0af44444f93013f42424932c3c0a
coder-nik200 <nitishkb1707@gmail.com>
2026-05-02 10:32:08 +0530

    Fix Firemote remote platform loading

--- custom_components/firemote/__init__.py
+++ custom_components/firemote/__init__.py
@@ -20,8 +20,8 @@ from .const import (
     CONF_MEDIA_PLAYER_ID,
     CONF_REMOTE_ID,
     CONF_ROKU_REMOTE_ID,
-    DOMAIN,
     DEFAULT_COMPATIBILITY_MODE,
+    DOMAIN,
     SERVICE_LAUNCH_APP,
     SERVICE_SEND_COMMAND,
     SERVICE_SEND_TEXT,
@@ -31,6 +31,9 @@ from .const import (
 )
 
 _LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = [Platform.REMOTE]
+
 
 DEVICE_SCHEMA = vol.Schema(
     {
@@ -42,7 +45,10 @@ DEVICE_SCHEMA = vol.Schema(
         vol.Optional(CONF_ROKU_REMOTE_ID): cv.entity_id,
         vol.Optional(CONF_APPLE_TV_REMOTE_ID): cv.entity_id,
         vol.Optional(CONF_DEVICE_TYPE): cv.string,
-        vol.Optional(CONF_COMPATIBILITY_MODE, default=DEFAULT_COMPATIBILITY_MODE): cv.string,
+        vol.Optional(
+            CONF_COMPATIBILITY_MODE,
+            default=DEFAULT_COMPATIBILITY_MODE,
+        ): cv.string,
     }
 )
 
@@ -50,14 +56,15 @@ CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
-                vol.Required(CONF_DEVICES): vol.All(cv.ensure_list, [DEVICE_SCHEMA])
+                vol.Required(CONF_DEVICES): vol.All(
+                    cv.ensure_list,
+                    [DEVICE_SCHEMA],
+                )
             }
         )
     },
     extra=vol.ALLOW_EXTRA,
 )
-
-PLATFORMS = [Platform.REMOTE]
 
 SERVICES = {
     SERVICE_SEND_COMMAND: {
@@ -77,7 +84,11 @@ SERVICES = {
         )
     },
     SERVICE_TOGGLE_POWER: {
-        "schema": vol.Schema({vol.Required(ATTR_DEVICE_NAME): cv.string})
+        "schema": vol.Schema(
+            {
+                vol.Required(ATTR_DEVICE_NAME): cv.string,
+            }
+        )
     },
     SERVICE_SET_VOLUME: {
         "schema": vol.Schema(
@@ -97,59 +108,79 @@ SERVICES = {
     },
 }
 
-PLATFORMS = []
-
 
 def _find_device(config, device_name):
     devices = config.get(CONF_DEVICES, [])
+
     for device in devices:
-        if device_name == device.get(CONF_NAME):
+        if device_name in (
+            device.get(CONF_NAME),
+            device.get(CONF_MEDIA_PLAYER_ID),
+            device.get(CONF_REMOTE_ID),
+            device.get(CONF_ANDROID_TV_REMOTE_ID),
+            device.get(CONF_ROKU_REMOTE_ID),
+            device.get(CONF_APPLE_TV_REMOTE_ID),
+        ):
             return device
-        if device_name == device.get(CONF_MEDIA_PLAYER_ID):
-            return device
-        if device_name == device.get(CONF_REMOTE_ID):
-            return device
-        if device_name == device.get(CONF_ANDROID_TV_REMOTE_ID):
-            return device
-        if device_name == device.get(CONF_ROKU_REMOTE_ID):
-            return device
-        if device_name == device.get(CONF_APPLE_TV_REMOTE_ID):
-            return device
+
     return None
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up Firemote integration."""
     hass.data[DOMAIN] = config.get(DOMAIN, {})
+
+    await hass.helpers.discovery.async_load_platform(
+        Platform.REMOTE,
+        DOMAIN,
+        {},
+        config,
+    )
 
     async def async_service_handler(service):
         device_name = service.data[ATTR_DEVICE_NAME]
         device_config = _find_device(hass.data[DOMAIN], device_name)
+
         if device_config is None:
-            _LOGGER.warning("Firemote service call target not found: %s", device_name)
+            _LOGGER.warning(
+                "Firemote target device not found: %s",
+                device_name,
+            )
             return
 
         from .adapter import FiremoteAdapter
 
         adapter = FiremoteAdapter(
-            hass,
-            device_config[CONF_MEDIA_PLAYER_ID],
-            device_config[CONF_DEVICE_FAMILY],
+            hass=hass,
+            entity_id=device_config[CONF_MEDIA_PLAYER_ID],
+            device_family=device_config[CONF_DEVICE_FAMILY],
             remote_id=device_config.get(CONF_REMOTE_ID),
-            android_tv_remote_id=device_config.get(CONF_ANDROID_TV_REMOTE_ID),
-            apple_tv_remote_id=device_config.get(CONF_APPLE_TV_REMOTE_ID),
+            android_tv_remote_id=device_config.get(
+                CONF_ANDROID_TV_REMOTE_ID
+            ),
+            apple_tv_remote_id=device_config.get(
+                CONF_APPLE_TV_REMOTE_ID
+            ),
             roku_remote_id=device_config.get(CONF_ROKU_REMOTE_ID),
             device_type=device_config.get(CONF_DEVICE_TYPE),
-            compatibility_mode=device_config.get(CONF_COMPATIBILITY_MODE, DEFAULT_COMPATIBILITY_MODE),
+            compatibility_mode=device_config.get(
+                CONF_COMPATIBILITY_MODE,
+                DEFAULT_COMPATIBILITY_MODE,
+            ),
         )
 
         if service.service == SERVICE_SEND_COMMAND:
             await adapter.send_command(service.data[ATTR_COMMAND])
+
         elif service.service == SERVICE_LAUNCH_APP:
             await adapter.launch_app(service.data["app_name"])
+
         elif service.service == SERVICE_TOGGLE_POWER:
             await adapter.toggle_power()
+
         elif service.service == SERVICE_SET_VOLUME:
             await adapter.set_volume(service.data[ATTR_VOLUME_LEVEL])
+
         elif service.service == SERVICE_SEND_TEXT:
             await adapter.send_text(service.data[ATTR_TEXT])
 
@@ -161,4 +192,5 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             schema=service_info["schema"],
         )
 
+    _LOGGER.info("Firemote integration loaded successfully")
     return True
--- custom_components/firemote/remote.py
+++ custom_components/firemote/remote.py
@@ -1,9 +1,15 @@
 import logging
 
-from homeassistant.components.remote import RemoteEntity, SUPPORT_SEND_COMMAND, SUPPORT_TURN_ON, SUPPORT_TURN_OFF
+from homeassistant.components.remote import (
+    RemoteEntity,
+    SUPPORT_SEND_COMMAND,
+    SUPPORT_TURN_OFF,
+    SUPPORT_TURN_ON,
+)
 from homeassistant.const import CONF_NAME
 from homeassistant.helpers.typing import ConfigType
 
+from .adapter import FiremoteAdapter
 from .const import (
     CONF_ANDROID_TV_REMOTE_ID,
     CONF_APPLE_TV_REMOTE_ID,
@@ -17,45 +23,74 @@ from .const import (
     DEFAULT_COMPATIBILITY_MODE,
     DOMAIN,
 )
-from .adapter import FiremoteAdapter
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_platform(hass, config: ConfigType, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass,
+    config: ConfigType,
+    async_add_entities,
+    discovery_info=None,
+):
+    """Set up Firemote remote entities."""
     device_configs = config.get(DOMAIN, {}).get(CONF_DEVICES, [])
+
     entities = []
 
     for device_config in device_configs:
         adapter = FiremoteAdapter(
-            hass,
-            device_config[CONF_MEDIA_PLAYER_ID],
-            device_config[CONF_DEVICE_FAMILY],
+            hass=hass,
+            entity_id=device_config[CONF_MEDIA_PLAYER_ID],
+            device_family=device_config[CONF_DEVICE_FAMILY],
             remote_id=device_config.get(CONF_REMOTE_ID),
-            android_tv_remote_id=device_config.get(CONF_ANDROID_TV_REMOTE_ID),
-            apple_tv_remote_id=device_config.get(CONF_APPLE_TV_REMOTE_ID),
+            android_tv_remote_id=device_config.get(
+                CONF_ANDROID_TV_REMOTE_ID
+            ),
+            apple_tv_remote_id=device_config.get(
+                CONF_APPLE_TV_REMOTE_ID
+            ),
             roku_remote_id=device_config.get(CONF_ROKU_REMOTE_ID),
             device_type=device_config.get(CONF_DEVICE_TYPE),
-            compatibility_mode=device_config.get(CONF_COMPATIBILITY_MODE, DEFAULT_COMPATIBILITY_MODE),
+            compatibility_mode=device_config.get(
+                CONF_COMPATIBILITY_MODE,
+                DEFAULT_COMPATIBILITY_MODE,
+            ),
         )
-        entities.append(FiremoteRemote(device_config[CONF_NAME], adapter))
+
+        entities.append(
+            FiremoteRemote(
+                device_config[CONF_NAME],
+                adapter,
+            )
+        )
 
     if entities:
         async_add_entities(entities, True)
 
 
 class FiremoteRemote(RemoteEntity):
-    def __init__(self, name: str, adapter: FiremoteAdapter) -> None:
+    """Firemote remote entity."""
+
+    def __init__(self, name: str, adapter: FiremoteAdapter):
         self._name = name
         self._adapter = adapter
 
     @property
-    def name(self) -> str:
+    def name(self):
         return self._name
 
     @property
-    def supported_features(self) -> int:
-        return SUPPORT_SEND_COMMAND | SUPPORT_TURN_ON | SUPPORT_TURN_OFF
+    def unique_id(self):
+        return f"firemote_{self._name.lower().replace(' ', '_')}"
+
+    @property
+    def supported_features(self):
+        return (
+            SUPPORT_SEND_COMMAND
+            | SUPPORT_TURN_ON
+            | SUPPORT_TURN_OFF
+        )
 
     @property
     def extra_state_attributes(self):
@@ -66,15 +101,12 @@ class FiremoteRemote(RemoteEntity):
         }
 
     async def async_send_command(self, command, **kwargs):
-        if isinstance(command, list):
-            commands = command
-        else:
-            commands = [command]
+        if not isinstance(command, list):
+            command = [command]
 
-        for command_item in commands:
-            if not command_item:
-                continue
-            await self._adapter.send_command(str(command_item))
+        for cmd in command:
+            if cmd:
+                await self._adapter.send_command(str(cmd))
 
     async def async_turn_on(self, **kwargs):
         await self._adapter.toggle_power()
@@ -82,10 +114,18 @@ class FiremoteRemote(RemoteEntity):
     async def async_turn_off(self, **kwargs):
         await self._adapter.toggle_power()
 
-    async def async_play_media(self, media_type: str, media_id: str, **kwargs):
+    async def async_play_media(
+        self,
+        media_type: str,
+        media_id: str,
+        **kwargs,
+    ):
         if media_type == "app":
             await self._adapter.launch_app(media_id)
         elif media_type == "source":
             await self._adapter.switch_source(media_id)
         else:
-            _LOGGER.debug("Unsupported media_type for Firemote remote: %s", media_type)
+            _LOGGER.debug(
+                "Unsupported media type: %s",
+                media_type,
+            )
